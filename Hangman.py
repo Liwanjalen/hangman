fc4cbec23649f3c30f5d9d2e6f2e07b3658fd1b5
Jalen <liwanjalenzki@gmail.com>
2022-07-24 23:24:35 +0800

    my first commit

---
+++ Hangman.py
@@ -0,0 +1,35 @@
+def hangman():
+    word = "motivated"
+    wrong_guesses = 0
+    phases = ["",
+             "_________",
+             "|    |   ",
+             "|    0   ",
+             "|   /|\  ",
+             "|   / \  ",
+             "|        "]
+    win = False
+    letters_left = list(word)
+    print("Welcome to Hangman!")
+    score_board = ["_"]*len(word)
+    while wrong_guesses < len(phases)-1:
+        print('\n')
+        guess = input("Guess a Letter:\n")
+        if guess in letters_left:
+            character_index = letters_left.index(guess)
+            score_board[character_index] = guess
+            letters_left[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print(''.join(score_board))
+        print('\n'.join(phases[0:wrong_guesses+1]))
+        if '_' not in score_board:
+            print('You win! The word was:')
+            print(''.join(score_board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(phases[0:wrong_guesses]))
+        print('You lose!')
+
+hangman()
